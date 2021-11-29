3fe2ea2ee485b09b953f4eac227a19eceb504a3c
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2021-11-30 02:03:14 +0300

    buton

--- resources/views/layouts/anasayfa.blade.php
+++ resources/views/layouts/anasayfa.blade.php
@@ -1,15 +1,10 @@
 @extends('layouts.anasayfadüzen')
 @section('content')
 <body>
-  <button type="button" name="button" onclick="location='ogrencianasayfa'">
-    Öğrenci Giriş
-  </button>
-  <button type="button" name="button" onclick="location='Admin'">
-    Admin Giriş
-  </button>
+    <link rel="stylesheet" href="{{ asset('css/a.css') }}" type="text/css">
+<div class="but">
+    <button type="button" name="button" class="button" onclick="location='ogrencianasayfa'"> Öğrenci Giriş  </button>
+  <button type="button" name="button" class="button" onclick="location='Admin'">  Admin Giriş </button>
 </body>
-
-
-
-
+</div>
 @stop
