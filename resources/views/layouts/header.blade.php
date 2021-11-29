db1fdbcd0a7a8a0117cb23755d8cb6980b4f24a0
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2021-11-29 22:37:39 +0300

    mehtap

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,5 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/a.css')}}">
   </head>
+  <body>
+    <div class="üst">
+fdgdhgfghdrdghtrhf
+    </div>
+  </body>
 </html>
