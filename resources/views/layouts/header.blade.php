6b3d2b5ddf84bdc64df6f3c4683a8d3138e3bc22
zeyniep <75210764+zeyniep@users.noreply.github.com>
2021-11-29 22:36:35 +0300

    yan menü

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,5 +3,6 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="ilk.css">
   </head>
 </html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/ilk.css') }}" type="text/css">
+  </head>
+
+  <body>
+    <div >
+    <ul >
+
+      <li><a href="#index">Ana sayfa</a></li>
+
+      <li><a href="#">Başvurularım</a></li>
+
+      <li><a href="#">Başvuru Yap</a></li>
+
+      <li><a href="#">Kişisel Bilgiler</a></li>
+
+      <li><a href="#">Çıkış</a></li>
+    </ul>
+  </div>
+  </body>
+</html>
