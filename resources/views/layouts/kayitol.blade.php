91e8b0ef82986e4d5cc812b24e7953bae58e9e3e
Mehtap <75047473+MehtapAyall@users.noreply.github.com>
2021-11-30 19:36:17 +0300

    sifre

--- resources/views/layouts/kayitol.blade.php
+++ resources/views/layouts/kayitol.blade.php
@@ -12,6 +12,7 @@
     <ul ><left>
       <li><a href="/kayitol">Kayıt Ol</a></li>
       <li><a href="/ogrencigiris">Giriş Yap</a></li>
+      <li><a href="/">Şifremi Unuttum</a></li>
     </ul>
   </div>
 
